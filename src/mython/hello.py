46f9320f4c92a84edb0c1d86082a4e4810341eed
ymat2 <ymat2@users.noreply.github.com>
2025-01-07 22:48:22 +0900

    :wrench: Show version

--- src/mython/hello.py
+++ src/mython/hello.py
@@ -1,9 +1,27 @@
+import argparse
 import os
+
+from importlib import metadata
+
+
+__version__ = metadata.version("mython")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Personal python package.")
+    parser.add_argument("-v", "--version", action="version", version=__version__, help="Show the version number")
+    parser.add_argument("-u", "--user", help="User name.")
+    args = parser.parse_args()
+    return args
 
 
 def main():
-    users = ["USER", "USERNAME", "user"]
-    user = next((os.environ.get(k) for k in users if k in os.environ), "anonymous")
+    args = parse_args()
+    if args.user:
+        user = args.user
+    else:
+        users = ["USER", "USERNAME", "user"]
+        user = next((os.environ.get(k) for k in users if k in os.environ), "anonymous")
     print("Hello, " + user)
 
 
